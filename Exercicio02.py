fbe402802940e33632721c5c0cb9e388fa44523a
Adyla Souza <adyla.projetos@gmail.com>
2025-04-28 14:52:04 -0300

    segunda atividade

---
+++ Exercicio02.py
@@ -0,0 +1,6 @@
+arrayAlunos = ['','','','','']
+for x in range (len(arrayAlunos)):
+    arrayAlunos[x]=input("Digite o nome de 5  alunos:  ")
+
+for y in range (len(arrayAlunos)):
+    print(f"{arrayAlunos[y]},está na posição {y} ")
