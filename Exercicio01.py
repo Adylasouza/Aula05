d038f13d242db48ebf98f8aac265d100d6dc03ad
Adyla Souza <adyla.projetos@gmail.com>
2025-04-28 14:46:57 -0300

    first atividade

---
+++ Exercicio01.py
@@ -0,0 +1,17 @@
+#array no java se escreve: string Nome[]= new String [x(quantidade/valor)]
+#lista no pyton: variavel = []
+#arrayCompras = ['banana', 'laranja','maçã']
+#for i in arrayCompras:
+ #   print(i)
+        #ou
+#for in range(len(frutas)):
+    #print(frutas[x])
+        #ou
+#for y in F rutas:
+    #print(y)
+#frutas.append("Tomate") | Adiciona item no final da lista
+    #print(frutas)
+arrayAlunos = ['','','','','']
+for x in range (len(arrayAlunos)):
+    arrayAlunos[x]=input("Digite o nome de 5  alunos:  ")
+print(arrayAlunos, end=" ")
