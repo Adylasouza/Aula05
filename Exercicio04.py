45d74cf58f35786441926a7bd87fe98b007b9f16
Adyla Souza <adyla.projetos@gmail.com>
2025-04-28 16:00:48 -0300

    quarta atividade

---
+++ Exercicio04.py
@@ -0,0 +1,14 @@
+notas = ['']*5
+soma = 0
+cont = 0
+for x in range (len(notas)):
+    notas[x] = float(input("Digite a nota: "))
+
+for y in range (len(notas)):
+    soma+=notas[y]
+media= soma/len(notas)
+
+for i in range (len(notas)):
+    if notas[i]>media:
+        cont+=1
+print(cont)
